58277d830fa556cd316b8c49a6784f268360f8bf
Chris <chriar10@uci.edu>
2021-10-11 15:12:54 -0700

    class def

---
+++ src/AcademicRecords.java
@@ -0,0 +1,2 @@
+public class AcademicRecords {
+}
--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,27 @@
+import java.io.IOException;
+
 public class Student {
+
+    /*----------------Variables-------------------------*/
+    private String name;
+
+    /**
+     * Constructor
+     * @param Input_file
+     * @throws IOException
+     */
+    public Student(String Input_file) throws IOException {
+
+
+    }
+    public void write(String Output_file) throws IOException{
+
+    }
+    /*------------------Setters--------------------------*/
+
+
+
+    /*------------------Getters--------------------------*/
+
+
 }
